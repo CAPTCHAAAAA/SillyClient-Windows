7529b08a8d1cd5b9e520244d6575496caa0626f2
CAPTCHA <bummer0624@gmail.com>
2026-07-12 21:34:14 +0800

    clean: 删除 bootstrap 残留脚本

--- bootstrap/scripts/start-server.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-cd /d "%TARVEN_SERVER_DIR%"
-set HOME=%TARVEN_HOME%
-set TMPDIR=%TARVEN_TMP%
-set NODE_ENV=production
-set AUTO_LAUNCH=false
-set NO_BROWSER=true
-"%TARVEN_NODE%" server.js
